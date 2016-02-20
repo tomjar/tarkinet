200dd7ffd1384bccd456ee98162fa7680870b8c9
tomjar <tomreese86@gmail.com>
2016-02-20 02:15:47 -0600

    removed readme, fixed path variables, added local jquery

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,16 +6,16 @@
 				<title></title>
 				<meta name="description" content="">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
-				<link rel="icon" href="../application/views/favicon.ico" type="image/x-icon">
+				<link rel="icon" href="../favicon.ico" type="image/x-icon">
 				<link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-				<link rel="stylesheet" href="../application/views/css/normalize.css">
-				<link rel="stylesheet" href="../application/views/css/main.css">
-				<link rel="stylesheet" href="../application/views/css/bootstrap.css">
-				<link rel="stylesheet" href="../application/views/css/bootstrap-theme.min.css" >
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-				<script src="../application/views/js/vendor/modernizr-2.8.3.min.js"></script>
-				<script src="../application/views/js/bootstrap.js"></script>
+				<link rel="stylesheet" href="../css/normalize.css">
+				<link rel="stylesheet" href="../css/main.css">
+				<link rel="stylesheet" href="../css/bootstrap.css">
+				<link rel="stylesheet" href="../css/bootstrap-theme.min.css" >
+				<script src="../js/jquery-1.12.0.js"></script>
+				<script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+				<script src="../js/bootstrap.js"></script>
         </head>
         <body>
 
